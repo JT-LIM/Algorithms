b3b8fc00ed979c7ba55cef50161d77d95073333f
JT-LIM <144074956+JT-LIM@users.noreply.github.com>
2023-09-12 21:36:44 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    var answer = [];
+    
+    var sortedS = s.slice(2, -2).split("},{")
+                    .map(item => item.split(",").map(Number))
+                    .sort((a, b) => a.length - b.length)
+    
+    for (var arr of sortedS) {
+        for (var num of arr) {
+            if(!answer.includes(num)) {
+                answer.push(num);
+            }
+        }
+    }
+               
+    
+    
+    
+    return answer;
+}
